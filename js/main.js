4be7bdaebbae190d5dea5a9870bb8ef422506fdd
StormKing969 <wijesinghesaj@gmail.com>
2021-12-02 15:33:46 -0500

    Mobile View Complete (12/02/2021)

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
 const slider = $("#myRange");
 const output = $("#output");
+const btn = $(".myCheck");
 
 slider.on("input", e => {
     let outputText = "$" + $(e.target).val() 
     output.html(outputText);
 })
+let count = 0;
+
+$(".slider2").click(e => {
+    if ($(this)) {
+        count += 1
+        if (count % 2 != 0) {
+            $(".slider2").addClass("sliderChecked_bg");
+        }
+        else {
+            $(".slider2").removeClass("sliderChecked_bg");
+        }
+    }
+});
